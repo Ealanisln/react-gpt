f43160798b6322a070c0d11285151f8bb74bbbd2
Emmanuel Alanis <ealanisln@me.com>
2024-01-07 17:47:19 -0800

    :sparkles: Added stream text

--- src/presentation/pages/pros-cons/ProsConsPage.tsx
+++ src/presentation/pages/pros-cons/ProsConsPage.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import { useState } from "react";
+import {
+  GptMessage,
+  MyMessage,
+  TextMessageBox,
+  TypingLoader,
+} from "../../components";
+import { prosConsUseCase } from "../../../core/use-cases"
 
-const ProsConsPage = () => {
-  return (
-    <div>ProsConsPage</div>
-  )
+interface Message {
+  text: string;
+  isGpt: boolean;
+  info?: {
+    errors: string[];
+    message: string;
+  }
 }
 
-export default ProsConsPage
+const ProsConsPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handlePost = async (text: string) => {
+    setIsLoading(true);
+    setMessages((prev) => [...prev, { text: text, isGpt: false }]);
+
+    // Todo UseCase
+
+    const { ok, content } = await prosConsUseCase(text);
+    setIsLoading(false);
+
+    if (!ok ) return;
+
+    setMessages((prev) => [...prev, { text: content, isGpt: true }]);
+
+  };
+
+  return (
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+          {/* Bienvenida  */}
+          <GptMessage text="Hola, puedes responder con lo que quieres que te ayude a comparar." />
+          {messages.map((message, index) =>
+            message.isGpt ? (
+              <GptMessage key={index} text={message.text}/>
+            ) : (
+              <MyMessage key={index} text={message.text} />
+            )
+          )}
+
+          {isLoading && (
+            <div className="col-start-1 col-end-12 fade-in">
+              <TypingLoader className="fade-in" />
+            </div>
+          )}
+        </div>
+      </div>
+
+      <TextMessageBox
+        onSendMessage={handlePost}
+        placeholder="Escribe aqui tu consulta"
+        disableCorrections
+      />
+    </div>
+  );
+};
+
+export default ProsConsPage;
